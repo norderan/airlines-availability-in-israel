b9608fc8ae56d27cc17a9aa9ae466c080736591d
norderan <queider@proton.me>
2025-06-20 11:12:10 +0300

    Add 404 page

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import styles from "./PageNotFound.module.css";
-import { Rocket } from "lucide-react";
+import styles from "./PageNotFound.module.scss";
 
 const PageNotFound = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <Rocket className={styles.icon} size={64} />
-        <h1 className={styles.title}>404</h1>
-        <p className={styles.subtitle}>Oops! Page Not Found</p>
-        <p className={styles.message}>
-          It looks like the page you are looking for has taken a trip to another
-          galaxy.
-        </p>
-        <a href="/" className={styles.homeLink}>
-          Go Back Home
-        </a>
-      </div>
+    <div className={styles.mainContainer}>
+      <h1>404</h1>
     </div>
   );
 };
--- src/router.jsx
+++ src/router.jsx
@@ -7,12 +7,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement: <PageNotFound />, // Catch errors specifically for routes under this layout
-  },
-  // Routes outside the main layout (e.g., login, standalone pages)
-  {
-    path: "*", // Catch-all route for any path not defined above
-    element: <PageNotFound />,
+    errorElement: <PageNotFound />,
   },
 ]);
 
